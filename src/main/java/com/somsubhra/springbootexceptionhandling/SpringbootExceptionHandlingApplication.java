39cf687f32f873618f02c9395f131160ceed2b14
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-28 20:52:11 +0530

    initial commit and boiler plate setup

---
+++ src/main/java/com/somsubhra/springbootexceptionhandling/SpringbootExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.somsubhra.springbootexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExceptionHandlingApplication.class, args);
+    }
+
+}
